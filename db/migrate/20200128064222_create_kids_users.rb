2b2a9b08a9be3b7daa3a96a65d13bd58ce7e0cd0
Masaru-0816 <two-of-us.0816@hotmail.co.jp>
2020-01-28 16:11:16 +0900

    created kids_users model

---
+++ db/migrate/20200128064222_create_kids_users.rb
@@ -0,0 +1,10 @@
+class CreateKidsUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :kids_users do |t|
+      t.references :user, foreign_key: true
+      t.references :kid, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
---
+++ test/models/kids_user_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class KidsUserTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
