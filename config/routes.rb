3ec4b8264419f56f8af620378cf8c140aad8caf6
Masaru-0816 <two-of-us.0816@hotmail.co.jp>
2020-01-29 13:38:21 +0900

    revised login logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  
   root 'top#index'
   
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:new, :create, :edit, :update, :destroy]
   resources :kids, only: [:create, :index, :edit, :update] do
     resources :notices, only: [:index, :create]
   end
