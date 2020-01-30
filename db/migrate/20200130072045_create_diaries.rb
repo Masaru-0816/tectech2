6c20160a75a5d5700f6c0db883ce4d5fa64db5dc
Masaru-0816 <two-of-us.0816@hotmail.co.jp>
2020-01-30 19:26:27 +0900

    initial commit

--- db/migrate/20200130072045_create_diaries.rb
+++ db/migrate/20200130072045_create_diaries.rb
@@ -1,8 +1,10 @@
 class CreateDiaries < ActiveRecord::Migration[5.2]
   def change
     create_table :diaries do |t|
-      t.
-
+      t.string :dinner_content
+      t.string :dinner_amount
+      t.string :breakfast_content
+      t.string :breakfast_amount
       t.timestamps
     end
   end
