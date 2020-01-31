b77b6820a1bdb8525b9e0b57fe7e6b578fd6eeac
Masaru-0816 <two-of-us.0816@hotmail.co.jp>
2020-01-31 13:22:37 +0900

    add meal text input box

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -2,7 +2,25 @@ class DiariesController < ApplicationController
   before_action :set_kid
 
   def new
-    @diary = Diary.includes(:kid)
+    @diary = Diary.new
+    @diaries = @kid.diaries.includes(:user)
+  end
+
+  def create
+    @diary = @kid.diaries.new(diary_params)
+    if @diary.save
+      redirect_to new_kid_diary, notice: "日誌を登録しました"
+    else
+      @diaries = @kid.diaries.includes(:user)
+      flash.now[:alert] = "情報を入力してください"
+      render :new
+    end
+  end
+
+  private
+
+  def diary_params
+      params.require(:diary).permit(:dinner_content)
   end
 
   def set_kid
