e2727479cddd1d4c50147b9acd30c64528d06db6
Masaru-0816 <two-of-us.0816@hotmail.co.jp>
2020-03-06 11:26:31 +0900

    chaned admin kids controller

--- app/controllers/admin/kids_controller.rb
+++ app/controllers/admin/kids_controller.rb
@@ -2,8 +2,10 @@ class Admin::KidsController < ApplicationController
   before_action :if_not_admin
 
   def index
+    @kid = Kid.new
     @kids = Kid.all
     @groups = Group.all
+    @user = User.all
   end
 
   def new
@@ -13,8 +15,9 @@ class Admin::KidsController < ApplicationController
   def create
     @kid = Kid.new(kid_params)
     if @kid.save
-      redirect_to admin_kids_path, notice: "児童情報を作成しました"
+      redirect_to new_admin_kid_path, notice: "児童情報を作成しました"
     else
+      flash.now[:alert] = "情報を入力してください"
       render :index
     end
   end
@@ -34,7 +37,7 @@ class Admin::KidsController < ApplicationController
   
   private
   def kid_params
-    params.require(:kid).permit(:name, :sex, :group_id, :image, :user_id)
+    params.require(:kid).permit(:name, :sex, :group_id, :image, users_attributes: [:name])
   end
 
   def if_not_admin
